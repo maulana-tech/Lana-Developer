78348fd8c4294483e25bb405ce0c95a262c54a14
maulana-tech <firdaussyah03@gmail.com>
2025-02-10 13:49:33 +0700

    Update personal information in layout and page components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,9 +11,9 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "John Doe - Developer",
+  title: "Maulana - Developer",
   description:
-    "John Doe is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
+    "Muhammad Maulana Firdaussyah is a developer. He is a digital nomad and travels around the world while working remotely.",
 };
 
 export default function RootLayout({
