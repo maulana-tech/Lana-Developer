cdc02075d0c281e03fa2a3c2f517f05a273693a1
maulana-tech <firdaussyah03@gmail.com>
2025-04-05 20:13:46 +0700

    new project

--- lana-portfolio/src/app/layout.tsx
+++ lana-portfolio/src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <MainLayout>{children}</MainLayout>
         </ThemeProvider>
       </body>
